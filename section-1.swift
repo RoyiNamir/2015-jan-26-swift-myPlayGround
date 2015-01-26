83478bd5cdcef263f7a5b170e636c571929f5592
HACKERU <hackeru@HACKERUs-Mac-2.local>
2015-01-26 10:57:47 -0800

    1
    1

---
+++ section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playgrou0nd"
+
+str+="sdfsdfs"
+
+let s1="hello" ;let s2="3";
+
+var s3:String = "hellow"
+
+//String,Int, Float , Double , Bool
+
+let s:String = "Hi there"
+let i:Int = -25
+let f:Float = 3.14
+let d:Double = 99.99
+let b:Bool = true
+
+
+
+
+
